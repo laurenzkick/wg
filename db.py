7678f1cad83f548409fb9f2d09ff194993a2cd24
laurenz.kick <laurenz.kick@tu-braunschweig.de>
2026-01-27 22:13:29 +0100

    print-diagnostic-1

--- db.py
+++ db.py
@@ -27,6 +27,7 @@ def close_db(e=None):
 
 def init_db():
     db = get_db()
+    print("INIT DB PATH (currentapp): " , current_app.config['DATABASE'])
 
     with current_app.open_resource('schema.sql') as f:
         db.executescript(f.read().decode('utf8'))
